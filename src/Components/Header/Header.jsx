59073fcd630efb5f92172fba3cfffa0b5f2537ab
Alec <alecgriffin@utexas.edu>
2017-10-18 16:47:41 -0500

    Changed Header Layout. Made Header reponsive

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -12,28 +12,32 @@ export default class Header extends Component {
             <Link to='/'>Game Day Ballers</Link>
           </Navbar.Brand>
         </Navbar.Header>
-        <Nav>
-          <LinkContainer to='/players'>
-            <NavItem eventKey={1} >Players</NavItem>
-          </LinkContainer>
+        <Navbar.Toggle />
 
-          <LinkContainer to='/teams'>
-            <NavItem eventKey={2} >Teams</NavItem>
-          </LinkContainer>
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <LinkContainer to='/players'>
+              <NavItem eventKey={1} >Players</NavItem>
+            </LinkContainer>
 
-          <LinkContainer to='/coaches'>
-            <NavItem eventKey={3} >Coaches</NavItem>
-          </LinkContainer>
+            <LinkContainer to='/teams'>
+              <NavItem eventKey={2} >Teams</NavItem>
+            </LinkContainer>
 
-          <LinkContainer to='/pre-nba'>
-            <NavItem eventKey={4} >Pre-NBA</NavItem>
-          </LinkContainer>
+            <LinkContainer to='/coaches'>
+              <NavItem eventKey={3} >Coaches</NavItem>
+            </LinkContainer>
 
-          <LinkContainer to='/about'>
-            <NavItem eventKey={5} >About</NavItem>
-          </LinkContainer>
+            <LinkContainer to='/pre-nba'>
+              <NavItem eventKey={4} >Pre-NBA</NavItem>
+            </LinkContainer>
 
-        </Nav>
+            <LinkContainer to='/about'>
+              <NavItem eventKey={5} >About</NavItem>
+            </LinkContainer>
+
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     );
   }
