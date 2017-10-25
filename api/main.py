348810fec2c4e23a1976694d746c7b8cc1a4905c
vikram <vikramidury@gmail.com>
2017-10-24 23:25:48 -0500

    Update routes in main.py

--- api/main.py
+++ api/main.py
@@ -1,28 +1,28 @@
 from flask import Flask
 from flask import jsonify
-from models import teams
+from models import teams, players, coaches, prenba
 
 app = Flask(__name__)
 
-@app.route('/healthcheck', methods=['GET'])
+@app.route('/', methods=['GET'])
 def healthcheck():
     return "GameDayBallers API is up and running!"
     
 @app.route('/players/', methods=['GET'])
 def list_players():
-    return "GameDayBallers API is up and running!"
+    return jsonify(players.list_players())
 
 @app.route('/players/<player_id>', methods=['GET'])
 def get_player_by_id(player_id):
-    return "GameDayBallers API is up and running!"
+    return jsonify(players.get_player_info(player_id))
 
 @app.route('/coaches/', methods=['GET'])
 def list_coaches():
-    return "GameDayBallers API is up and running!"
+    return jsonify(coaches.list_coaches())
 
 @app.route('/coaches/<coach_id>', methods=['GET'])
 def get_coach_by_id(coach_id):
-    return "GameDayBallers API is up and running!"
+    return jsonify(coaches.get_coach_info(coach_id))
     
 @app.route('/teams/', methods=['GET'])
 def list_teams():
@@ -34,9 +34,9 @@ def get_team_by_id(team_id):
     
 @app.route('/pre-nba/', methods=['GET'])
 def list_prenba():
-    return "GameDayBallers API is up and running!"
+    return jsonify(prenba.list_prenba())
     
 @app.route('/pre-nba/<pre_nba_id>', methods=['GET'])
 def get_prenba_by_id(pre_nba_id):
-    return "GameDayBallers API is up and running!"
+    return jsonify(prenba.get_prenba_info(pre_nba_id))
 
