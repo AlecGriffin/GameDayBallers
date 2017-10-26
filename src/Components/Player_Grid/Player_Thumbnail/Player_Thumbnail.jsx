dd722ebbc3297ee283d6b5a1ea0ff4c82caa35f9
Alec <alecgriffin@utexas.edu>
2017-10-26 13:24:21 -0500

    Fixed Players Page where no image is found for a player

--- src/Components/Player_Grid/Player_Thumbnail/Player_Thumbnail.jsx
+++ src/Components/Player_Grid/Player_Thumbnail/Player_Thumbnail.jsx
@@ -2,13 +2,23 @@ import React, {Component} from 'react'
 import { Grid, Row, Col, Image, Thumbnail } from 'react-bootstrap';
 
 export default class Player_Thumbnail extends Component {
+  addDefaultSrc(ev){
+    ev.target.src = 'https://dummyimage.com/260x190/9e9e9e/ffffff.png&text=No+Image+Found'
+  }
+
 
   render(){
+    // var image_url = this.props.src
+    // if(this.props.src === null || this.props.src === '')
+    //   image_url = "http://via.placeholder.com/250x190"
+
     return(
       <Col xs={6} sm={4} className="text-center">
           <div className="card image-card">
             <div className="card-title">
-              <img src={this.props.src}/>
+
+              <img onError={this.addDefaultSrc} className="img-responsive" src={this.props.src} alt='Wtf'/>
+
             </div>
             <div className="card-body">
               {this.props.name}
