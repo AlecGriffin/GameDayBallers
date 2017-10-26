59a2875bbfb356f7a48b1233b125795d40f3ff88
vikram <vikramidury@gmail.com>
2017-10-25 22:28:44 -0500

    Add GCP access code

--- api/db_helper.py
+++ api/db_helper.py
@@ -1,15 +1,41 @@
 import MySQLdb
+import os
 
 # Convenient access class for MySQL db cursor
 class db_connect:
     def __init__(self):
-        host = "localhost"
-        user = "root"
-        password = None
-        db = "gamedayballersdb"
-        self.db = MySQLdb.connect(host=host,    # your host, usually localhost
-                     user=user,                      # your username
-                     db=db)            # name of the data base
+        CLOUDSQL_CONNECTION_NAME = os.environ.get('CLOUDSQL_CONNECTION_NAME')
+        CLOUDSQL_USER = os.environ.get('CLOUDSQL_USER')
+        CLOUDSQL_PASSWORD = os.environ.get('CLOUDSQL_PASSWORD')
+
+        def connect_to_cloudsql():
+            # When deployed to App Engine, the `SERVER_SOFTWARE` environment variable
+            # will be set to 'Google App Engine/version'.
+            if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
+                # Connect using the unix socket located at
+                # /cloudsql/cloudsql-connection-name.
+                cloudsql_unix_socket = os.path.join(
+                    '/cloudsql', CLOUDSQL_CONNECTION_NAME)
+
+                db = MySQLdb.connect(
+                    unix_socket=cloudsql_unix_socket,
+                    user=CLOUDSQL_USER,
+                    passwd=CLOUDSQL_PASSWORD,
+                    db="gamedayballersdb")
+
+            # If the unix socket is unavailable, then try to connect using TCP. This
+            # will work if you're running a local MySQL server or using the Cloud SQL
+            # proxy, for example:
+            #
+            #   $ cloud_sql_proxy -instances=your-connection-name=tcp:3306
+            #
+            else:
+                db = MySQLdb.connect(
+                    host='localhost', user="root", db="gamedayballersdb")
+
+            return db
+
+        self.db = connect_to_cloudsql()
         self.cur = self.db.cursor()
 
     def __enter__(self):
