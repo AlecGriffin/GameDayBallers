7373e12a97eff92f3c03a894df13e32b9da80b46
Andrew Duna <aduna.393@gmail.com>
2017-11-07 20:27:29 -0600

    Add data for player hovers

--- src/Components/Player_Grid/Player_Thumbnail/Player_Thumbnail.jsx
+++ src/Components/Player_Grid/Player_Thumbnail/Player_Thumbnail.jsx
@@ -6,16 +6,30 @@ export default class Player_Thumbnail extends Component {
     ev.target.src = 'https://dummyimage.com/260x190/9e9e9e/ffffff.png&text=No+Image+Found'
   }
 
+  renderOverlay() {
+    if (this.props.overlay) {
+      return (<div className="overlay">
+        <div className="overlay-info">
+          <h2 class="overlay-info-title">{this.props.name} #{this.props.jerseyNumber}</h2>
+          <div className="overlay-info-body">
+            <p><b>Position: </b>{this.props.position}</p>
+            <p><b>Date of Birth: </b> {this.props.dob}</p>
+            <p><b>Height: </b> {this.props.height}</p>
+            <p><b>Weight: </b> {this.props.weight}</p>
+          </div>
+        </div>
+      </div>);
+    } else {
+      return (<div></div>);
+    }
+  }
+
   render(){
     return(
       <Col xs={6} md={4} className="text-center">
           <div className="card thumbnail-card image-card">
             <div className="card-title">
-              <div className="overlay">
-                <div className="overlay-info">
-                  {this.props.name}
-                </div>
-              </div>
+              { this.renderOverlay() }
               <img onError={this.addDefaultSrc} src={this.props.src} alt='Wtf'/ >
 
             </div>
