24dbc9767f5dad852bd204ffca6a59be60c2c5f9
Andrew Duna <aduna.393@gmail.com>
2017-11-08 21:04:06 -0600

    Implement filtering for all models

--- src/Components/Team_Grid/Team_Thumbnail/Team_Thumbnail.jsx
+++ src/Components/Team_Grid/Team_Thumbnail/Team_Thumbnail.jsx
@@ -2,22 +2,30 @@ import React, {Component} from 'react'
 import { Grid, Row, Col, Image, Thumbnail } from 'react-bootstrap';
 
 export default class Team_Thumbnail extends Component {
+  renderOverlay() {
+    if (this.props.overlay) {
+      return (<div className="overlay">
+        <div className="overlay-info">
+          <h2 class="overlay-info-title">{this.props.name}</h2>
+          <div className="overlay-info-body">
+            <p><b>City: </b>{this.props.city}</p>
+            <p><b>Arena: </b> {this.props.arena}</p>
+            <p><b>Conference: </b> {this.props.conference}</p>
+            <p><b>Division: </b> {this.props.division}</p>
+          </div>
+        </div>
+      </div>);
+    } else {
+      return (<div></div>);
+    }
+  }
+
   render(){
     return(
       <Col xs={6} sm={4} className="text-center">
           <div className="card thumbnail-card image-card white-card team-logo">
             <div className="card-title">
-              <div className="overlay">
-                <div className="overlay-info">
-                  <h2 class="overlay-info-title">{this.props.name}</h2>
-                  <div className="overlay-info-body">
-                    <p><b>City: </b>{this.props.city}</p>
-                    <p><b>Arena: </b> {this.props.arena}</p>
-                    <p><b>Conference: </b> {this.props.conference}</p>
-                    <p><b>Division: </b> {this.props.division}</p>
-                  </div>
-                </div>
-              </div>
+              { this.renderOverlay() }
               <img src={this.props.src}/>
             </div>
             <div className="card-body">
