60f7de8c88eba70626a1f2da3f8a013c548a60ac
Alec <alecgriffin@utexas.edu>
2017-11-26 22:24:24 -0600

    Added Loading Animation to Visualization Page

--- src/Components/Visualization/Visualization.jsx
+++ src/Components/Visualization/Visualization.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios'
 import {Text, ReferenceLine, Brush, RadialBar, RadialBarChart, Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, Pie, PieChart, Bar, BarChart, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts'
 import {Row, Col} from 'react-bootstrap'
+import Loading from '../Loading/Loading.jsx'
 
 
 export default class Visualization extends Component {
@@ -109,9 +110,10 @@ export default class Visualization extends Component {
     return(
       <div className='main'>
         <h1>Visualizations:</h1>
-      <Row>
-        <h2>Artists:</h2>
+        { !this.state.dataLoaded && <Loading/> }
         { this.state.dataLoaded && (
+          <Row>
+            <h2>Artists:</h2>
            <BarChart width={1200} height={800} data={this.state.artistData.sort((a,b) =>{return b.playcount - a.playcount})}
               margin={{top: 100, right: 30, left: 20, bottom: 100}}>
               <Brush dataKey='name' height={30} stroke="#ff7300"/>
@@ -122,61 +124,68 @@ export default class Visualization extends Component {
               <Legend verticalAlign="top" wrapperStyle={{lineHeight: '40px'}}/>
               <Bar dataKey="playcount" fill="#8884d8" />
            </BarChart>
+           </Row>
         )}
-      </Row>
 
-      <Row>
-        <h2>Albums:</h2>
+
+
+
         { this.state.dataLoaded && (
-          <BarChart width={1200} height={800} data={this.state.albumData.sort((a,b) =>{return b.playcount - a.playcount})}
-             margin={{top: 100, right: 30, left: 20, bottom: 100}}>
-             <XAxis height={180} angle={40} textAnchor="start" dataKey="name" interval={0} tickLine={false}/>
-             <YAxis/>
-             <CartesianGrid strokeDasharray="3 3"/>
-             <Tooltip/>
-             <Legend verticalAlign="top" wrapperStyle={{lineHeight: '40px'}}/>
+          <Row>
+            <h2>Albums:</h2>
+            <BarChart width={1200} height={800} data={this.state.albumData.sort((a,b) =>{return b.playcount - a.playcount})}
+               margin={{top: 100, right: 30, left: 20, bottom: 100}}>
+               <XAxis height={180} angle={40} textAnchor="start" dataKey="name" interval={0} tickLine={false}/>
+               <YAxis/>
+               <CartesianGrid strokeDasharray="3 3"/>
+               <Tooltip/>
+               <Legend verticalAlign="top" wrapperStyle={{lineHeight: '40px'}}/>
 
-             <Brush dataKey='name' height={30} stroke="#ff7300"/>
-             <Bar dataKey="playcount" fill="#8884d8" />
-          </BarChart>
+               <Brush dataKey='name' height={30} stroke="#ff7300"/>
+               <Bar dataKey="playcount" fill="#8884d8" />
+            </BarChart>
+          </Row>
 
         )}
-      </Row>
 
-      <Row>
-        <h2>Tracks:</h2>
+
+
+
         { this.state.dataLoaded && (
-          <BarChart width={1200} height={800} data={this.state.trackData.sort((a,b) =>{return b.playcount - a.playcount})}
-             margin={{top: 100, right: 30, left: 20, bottom: 100}}>
-             <XAxis height={180} angle={40} textAnchor="start" dataKey="name" interval={0} tickLine={false}/>
-             <YAxis/>
-             <CartesianGrid strokeDasharray="3 3"/>
-             <Tooltip/>
-             <Legend verticalAlign="top" wrapperStyle={{lineHeight: '40px'}}/>
+          <Row>
+            <h2>Tracks:</h2>
+            <BarChart width={1200} height={800} data={this.state.trackData.sort((a,b) =>{return b.playcount - a.playcount})}
+               margin={{top: 100, right: 30, left: 20, bottom: 100}}>
+               <XAxis height={180} angle={40} textAnchor="start" dataKey="name" interval={0} tickLine={false}/>
+               <YAxis/>
+               <CartesianGrid strokeDasharray="3 3"/>
+               <Tooltip/>
+               <Legend verticalAlign="top" wrapperStyle={{lineHeight: '40px'}}/>
 
-             <Brush dataKey='name' height={30} stroke="#ff7300"/>
-             <Bar dataKey="playcount" fill="#8884d8" />
-          </BarChart>
+               <Brush dataKey='name' height={30} stroke="#ff7300"/>
+               <Bar dataKey="playcount" fill="#8884d8" />
+            </BarChart>
+          </Row>
         )}
-      </Row>
 
-      <Row>
-        <h2>Playlists:</h2>
+
+
         { this.state.dataLoaded && (
-          <BarChart width={1200} height={800} data={this.state.playlistData.sort((a,b) =>{return b.numFollowers - a.numFollowers})}
-             margin={{top: 100, right: 30, left: 20, bottom: 100}}>
-             <XAxis height={180} angle={40} textAnchor="start" dataKey="name" interval={0} tickLine={false}/>
-             <YAxis/>
-             <CartesianGrid strokeDasharray="3 3"/>
-             <Tooltip/>
-             <Legend verticalAlign="top" wrapperStyle={{lineHeight: '40px'}}/>
+          <Row>
+            <h2>Playlists:</h2>
+            <BarChart width={1200} height={800} data={this.state.playlistData.sort((a,b) =>{return b.numFollowers - a.numFollowers})}
+               margin={{top: 100, right: 30, left: 20, bottom: 100}}>
+               <XAxis height={180} angle={40} textAnchor="start" dataKey="name" interval={0} tickLine={false}/>
+               <YAxis/>
+               <CartesianGrid strokeDasharray="3 3"/>
+               <Tooltip/>
+               <Legend verticalAlign="top" wrapperStyle={{lineHeight: '40px'}}/>
 
-             <Brush dataKey='name' height={30} stroke="#ff7300"/>
-             <Bar dataKey="numFollowers" fill="#8884d8" />
-          </BarChart>
+               <Brush dataKey='name' height={30} stroke="#ff7300"/>
+               <Bar dataKey="numFollowers" fill="#8884d8" />
+            </BarChart>
+          </Row>
         )}
-      </Row>
-
       </div>
     )
   }
